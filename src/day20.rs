7a08d7b990973fdfe25a45b7c11933f8a3b317ea
Roberto Ariosa <rob.ariosa@hotmail.com>
2022-12-20 10:55:37 +0100

    Simplify

--- src/day20.rs
+++ src/day20.rs
@@ -14,36 +14,18 @@ fn groove_coordinates(nums: &[i64], positions: &[usize]) -> i64 {
     nums[positions[index_1000th]] + nums[positions[index_2000th]] + nums[positions[index_3000th]]
 }
 
-pub fn mix(input: &str) -> i64 {
-    // [input index] = num
-    let nums = input.lines().map(|line| line.parse().unwrap()).collect::<Vec<i64>>();
-    // [final index] = input index
-    let mut positions = (0..nums.len()).collect::<Vec<usize>>();
-
-    for (input_index, num) in nums.iter().enumerate() {
-        let final_index = positions.iter().position(|&p| p == input_index).unwrap();
-        positions.remove(final_index);
-        let new_final_index = (final_index as i64 + num).rem_euclid(positions.len() as i64);
-        positions.insert(new_final_index as usize, input_index);
-    }
-
-    groove_coordinates(&nums, &positions)
-}
-
-pub fn mix_with_key(input: &str) -> i64 {
-    // Constants
-    const DECRYPTION_KEY: i64 = 811_589_153;
-    const ITERATIONS: usize = 10;
-
+fn _mix(input: &str, iterations: usize, decryption_key: i64) -> i64 {
     // [input index] = num
     let nums = input
         .lines()
-        .map(|line| line.parse::<i64>().unwrap() * DECRYPTION_KEY)
+        .map(|line| line.parse::<i64>().unwrap() * decryption_key)
         .collect::<Vec<i64>>();
+
     // [final index] = input index
     let mut positions = (0..nums.len()).collect::<Vec<usize>>();
 
-    for _ in 0..ITERATIONS {
+    for _ in 0..iterations {
+        // Mix
         for (input_index, num) in nums.iter().enumerate() {
             let final_index = positions.iter().position(|&p| p == input_index).unwrap();
             positions.remove(final_index);
@@ -52,7 +34,18 @@ pub fn mix_with_key(input: &str) -> i64 {
         }
     }
 
+    // Compute result
     groove_coordinates(&nums, &positions)
+}
+
+pub fn mix(input: &str) -> i64 {
+    _mix(input, 1, 1)
+}
+
+pub fn mix_with_key(input: &str) -> i64 {
+    const DECRYPTION_KEY: i64 = 811_589_153;
+    const ITERATIONS: usize = 10;
+    _mix(input, ITERATIONS, DECRYPTION_KEY)
 }
 
 pub fn main() {
