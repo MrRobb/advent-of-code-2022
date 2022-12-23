e3010fae62d3fec9e6b7e8b4830b8bf6d426d89c
Roberto Ariosa <roberto.ariosa@upc.edu>
2022-12-23 18:17:05 +0100

    Optimize

--- src/day23.rs
+++ src/day23.rs
@@ -2,7 +2,8 @@
     clippy::must_use_candidate,
     clippy::missing_panics_doc,
     clippy::cast_possible_truncation,
-    clippy::cast_possible_wrap
+    clippy::cast_possible_wrap,
+    clippy::cast_sign_loss
 )]
 
 use std::{ops::Add, option::Option};
@@ -10,7 +11,6 @@ use std::{ops::Add, option::Option};
 use enum_iterator::{all, cardinality, Sequence};
 use hashbrown::{HashMap, HashSet};
 use itertools::Itertools;
-use rayon::prelude::*;
 
 type ElfPosition = (i32, i32);
 
@@ -43,7 +43,34 @@ impl Add<Direction> for ElfPosition {
     }
 }
 
-fn simulate(input: &str, max_rounds: Option<usize>) -> i32 {
+fn neighbors(map: &HashSet<ElfPosition>, elf: ElfPosition) -> [bool; cardinality::<Direction>()] {
+    all::<Direction>()
+        .map(|d| map.contains(&(elf + d)))
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap()
+}
+
+fn propose(neighbors: [bool; cardinality::<Direction>()], round: usize) -> Option<Direction> {
+    if neighbors.iter().all(|n| !n) {
+        return None;
+    }
+
+    [Direction::North, Direction::South, Direction::West, Direction::East]
+        .into_iter()
+        .cycle()
+        .skip(round % 4)
+        .take(4)
+        .find(|d| match d {
+            Direction::North => !neighbors[0] && !neighbors[4] && !neighbors[5],
+            Direction::South => !neighbors[1] && !neighbors[6] && !neighbors[7],
+            Direction::East => !neighbors[2] && !neighbors[4] && !neighbors[6],
+            Direction::West => !neighbors[3] && !neighbors[5] && !neighbors[7],
+            _ => unreachable!(),
+        })
+}
+
+fn simulate(input: &str, max_rounds: Option<usize>) -> usize {
     // Build map
     let mut elves = input
         .lines()
@@ -56,34 +83,28 @@ fn simulate(input: &str, max_rounds: Option<usize>) -> i32 {
         })
         .collect::<HashSet<ElfPosition>>();
 
-    let mut round = 0;
-    loop {
+    for round in 0.. {
         // Generate proposals
-        let proposals = elves
-            .par_iter()
-            .map(|elf| {
-                let neighbors = neighbors(&elves, *elf);
-                (*elf, propose(&neighbors, round).map(|d| *elf + d))
-            })
-            .collect::<HashMap<ElfPosition, Option<ElfPosition>>>();
+        let mut all_proposals: HashMap<ElfPosition, Vec<ElfPosition>> = HashMap::new();
+        for elf in elves.iter() {
+            let neighbors = neighbors(&elves, *elf);
+            if let Some(proposal) = propose(neighbors, round).map(|d| *elf + d) {
+                all_proposals.entry(proposal).or_default().push(*elf);
+            }
+        }
 
-        // Remove collisions
+        // Move elves
         let mut someone_moved = false;
-        proposals
-            .par_iter()
-            .filter(|(_, p)| proposals.values().filter(|p2| p == p2).count() == 1)
-            .collect::<Vec<_>>()
-            .into_iter()
-            .for_each(|(elf, proposal)| {
-                if let Some(proposal) = proposal {
-                    elves.remove(elf);
-                    elves.insert(*proposal);
-                    someone_moved = true;
-                }
+        all_proposals
+            .iter()
+            // Filter collisions
+            .filter(|(_, candidates)| candidates.len() == 1)
+            // Move elf
+            .for_each(|(proposal, candidates)| {
+                elves.remove(&candidates[0]);
+                elves.insert(*proposal);
+                someone_moved = true;
             });
-
-        // Update round
-        round += 1;
 
         // Break if max rounds have been simulated (Part 1)
         if let Some(max_rounds) = max_rounds {
@@ -93,54 +114,22 @@ fn simulate(input: &str, max_rounds: Option<usize>) -> i32 {
         }
         // Break if noone moved (Part 2)
         else if !someone_moved {
-            return round as i32;
+            return round + 1;
         }
     }
 
     // Calculate bounds
     let (min_x, max_x) = elves.iter().map(|(x, _)| x).minmax().into_option().unwrap();
     let (min_y, max_y) = elves.iter().map(|(_, y)| y).minmax().into_option().unwrap();
-    (max_x - min_x + 1) * (max_y - min_y + 1) - elves.len() as i32
+    (max_x - min_x + 1) as usize * (max_y - min_y + 1) as usize - elves.len()
 }
 
-pub fn part1(input: &str) -> i32 {
+pub fn part1(input: &str) -> usize {
     simulate(input, Some(10))
 }
 
-pub fn part2(input: &str) -> i32 {
+pub fn part2(input: &str) -> usize {
     simulate(input, None)
-}
-
-fn neighbors(map: &HashSet<ElfPosition>, elf: ElfPosition) -> [Option<ElfPosition>; cardinality::<Direction>()] {
-    let mut neighbors = [None; cardinality::<Direction>()];
-    for (i, d) in all::<Direction>().enumerate() {
-        let neighbor = elf + d;
-        if map.contains(&neighbor) {
-            neighbors[i] = Some(neighbor);
-        }
-    }
-    neighbors
-}
-
-fn propose(neighbors: &[Option<ElfPosition>; cardinality::<Direction>()], round: usize) -> Option<Direction> {
-    [Direction::North, Direction::South, Direction::West, Direction::East]
-        .into_iter()
-        .cycle()
-        .skip(round % 4)
-        .take(4)
-        .find(|d| {
-            if neighbors.iter().all(Option::is_none) {
-                false
-            } else {
-                match d {
-                    Direction::North => neighbors[0].is_none() && neighbors[4].is_none() && neighbors[5].is_none(),
-                    Direction::South => neighbors[1].is_none() && neighbors[6].is_none() && neighbors[7].is_none(),
-                    Direction::East => neighbors[2].is_none() && neighbors[4].is_none() && neighbors[6].is_none(),
-                    Direction::West => neighbors[3].is_none() && neighbors[5].is_none() && neighbors[7].is_none(),
-                    _ => unreachable!(),
-                }
-            }
-        })
 }
 
 pub fn main() {
